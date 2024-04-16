0fcf7fc0b4afeaccedc35fe47985297455dfe030
Taha Shah <tahashah411@gmail.com>
2024-04-17 00:03:38 +0500

    scrollbar css

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -6,21 +6,23 @@ const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-          {/* <Image
-            src="/NavLogo.png"
-            alt="logo"
-            width={40}
-            height={40}
-            className="cursor-pointer hover:animate-slowspin"
-          /> */}
+          <div className="flex gap-2 items-end">
+            {/* <Image
+              src="/NavLogo.png"
+              alt="logo"
+              width={24}
+              height={24}
+              className="cursor-pointer hover:animate-slowspin"
+              /> */}
 
-          <div className="font-bold ml-[10px] hidden md:block text-gray-300 tracking-[0.5rem]">
-              Aztro
+            <div className="font-bold hidden md:flex w-[150px] text-gray-300 tracking-[0.5rem] items-center">
+                Aztro
+            </div>
           </div>
         
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between ml-5 md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+        <div className="h-full flex-1 flex flex-row items-center justify-center">
+          <div className=" w-[500px] flex items-center justify-between h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About me
             </a>
@@ -33,7 +35,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="flex flex-row gap-5">
+        <div className="flex gap-5 w-[150px] justify-end">
           {Socials.map((social) => (
             <Image
               src={social.src}
--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -63,7 +63,7 @@ const Skills = () => {
           />
         ))}
       </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+      {/* <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Other_skill.map((image, index) => (
           <SkillDataProvider
             key={index}
@@ -73,7 +73,7 @@ const Skills = () => {
             index={index}
           />
         ))}
-      </div>
+      </div> */}
 
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
