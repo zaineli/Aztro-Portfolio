8a72827b09da5f3d2b37d57271f7ac0ac80cc3d0
Abbas-Askari <abbasaskari797@gmail.com>
2024-04-17 16:14:46 +0500

    fixed z index issue

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -13,42 +13,41 @@ const StarBackground = (props: any) => {
   );
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta/10;
-    ref.current.rotation.y -= delta/15;
-  })
-
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
 
   return (
-    <group rotation={[0,0, Math.PI / 4]} >
-        <Points
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
         ref={ref}
         positions={sphere}
         stride={3}
         frustumCulled
         className="  pointer-events-none"
         {...props}
-        >
-            <PointMaterial
-        className="  pointer-events-none"
-        transparent
-                color="$fff"
-                size={0.002}
-                sizeAttenuation={true}
-                dethWrite={false}
-            />
-        </Points>
+      >
+        <PointMaterial
+          className="  pointer-events-none"
+          transparent
+          color="$fff"
+          size={0.002}
+          sizeAttenuation={true}
+          dethWrite={false}
+        />
+      </Points>
     </group>
-  )
+  );
 };
 
 const StarsCanvas = () => (
-    <div className="w-full h-auto fixed inset-0 z-[20] pointer-events-none">
-        <Canvas camera={{position: [0, 0, 1]}}>
-        <Suspense fallback={null}>
-            <StarBackground />
-        </Suspense>
-        </Canvas>
-    </div>
-)
+  <div className="w-full h-auto fixed inset-0 z-[-20] pointer-events-none">
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Suspense fallback={null}>
+        <StarBackground />
+      </Suspense>
+    </Canvas>
+  </div>
+);
 
 export default StarsCanvas;
